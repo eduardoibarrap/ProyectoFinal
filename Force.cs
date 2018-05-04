05c3deb1428d93d92b45c88627216c7ec72005fa
eduardoibarrap <36088097+eduardoibarrap@users.noreply.github.com>
2018-05-04 18:52:27 -0500

    Add files via upload

---
+++ Force.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Force : MonoBehaviour {
+
+	public float thrust=10;
+	public Rigidbody2D rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate()
+	{
+		Debug.Log ("F");
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			rb.AddForce (new Vector3 (25, 0, 0) * thrust);
+		}
+		Debug.Log ("F");
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			rb.AddForce (new Vector3 (-25, 0, 0) * thrust);
+		}
+		Debug.Log ("F");
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			rb.AddForce (new Vector3 (0, 20, 0) * thrust);
+		}
+		Debug.Log ("F");
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			transform.position = Vector3.zero;
+		}
+	}
+
+}
+
---
+++ destroy.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy : MonoBehaviour {
+
+
+
+	void OnCollisionEnter2D(Collision2D col){
+	  
+		if (col.gameObject.name == "caja1") {
+
+			Destroy (col.gameObject);
+
+		}
+		if (col.gameObject.name == "caja2") {
+
+			Destroy (col.gameObject);
+
+		}
+		if (col.gameObject.name == "caja3") {
+
+			Destroy (col.gameObject);
+
+		}
+
+		if (col.gameObject.name == "caja4") {
+
+			Destroy (col.gameObject);
+
+		}
+
+		if (col.gameObject.name == "caja5") {
+
+			Destroy (col.gameObject);
+
+		}
+		if (col.gameObject.name == "caja6") {
+
+			Destroy (col.gameObject);
+
+		}
+
+		if (col.gameObject.name == "caja7") {
+
+			Destroy (col.gameObject);
+
+		}
+
+		if (col.gameObject.name == "caja8") {
+
+			Destroy (col.gameObject);
+
+		}
+
+		if (col.gameObject.name == "caja9") {
+
+			Destroy (col.gameObject);
+
+		}
+
+		if (col.gameObject.name == "caja10") {
+
+			Destroy (col.gameObject);
+
+		}
+
+		if (col.gameObject.name == "caja11") {
+
+			Destroy (col.gameObject);
+
+		}
+
+		if (col.gameObject.name == "caja12") {
+
+			Destroy (col.gameObject);
+
+		}
+
+		if (col.gameObject.name == "caja13") {
+
+			Destroy (col.gameObject);
+
+		}
+
+		if (col.gameObject.name == "caja14") {
+
+			Destroy (col.gameObject);
+
+		}
+
+		if (col.gameObject.name == "caja15") {
+
+			Destroy (col.gameObject);
+
+		}
+
+		if (col.gameObject.name == "caja16") {
+
+			Destroy (col.gameObject);
+
+		}
+
+		if (col.gameObject.name == "caja17") {
+
+			Destroy (col.gameObject);
+
+		}
+		}
+	
+	}
+	
+
